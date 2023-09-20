bd7669288863f742b180a1f1c7122e6c7573ece8
seodevv <seodev17@gmail.com>
2023-09-20 11:04:19 +0900

    projects edit:

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,55 +1,46 @@
 import styled from 'styled-components';
 import { Header, Inner } from './Styled';
-import MINE_1 from '../img/minesweeper_1.png';
-import MINE_2 from '../img/minesweeper_2.png';
-import MINE_3 from '../img/minesweeper_3.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCaretRight,
-  faChevronLeft,
-  faChevronRight,
-  faCircle,
-  faGear,
-  faUser,
-} from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import { faCalendar } from '@fortawesome/free-regular-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import MineSweeper from './projects/MineSweeper';
+import Tikitaka from './projects/Tikitaka';
 
 const StyledProjects = styled.section`
   padding: 30px;
   background: #4b7e9c;
 `;
 
-const Project = styled.div`
+export const Project = styled.div`
   margin-top: 30px;
-  width: 50%;
+  width: 70%;
   background: #fff;
   border-radius: 15px;
+  overflow: hidden;
 `;
 
-const ImgSlide = styled.div`
+export const ImgSlide = styled.div`
   position: relative;
   display: flex;
   flex-flow: row nowrap;
   width: 100%;
+  height: ${(props) => (props.height ? props.height : 'auto')};
   overflow: hidden;
   user-select: none;
 
   img {
     min-width: 100%;
     transition: 0.3s all ease-in;
+    object-fit: contain;
   }
 `;
 
-const Slider = styled(FontAwesomeIcon)`
+export const Slider = styled(FontAwesomeIcon)`
   padding: 5px;
   position: absolute;
   top: 50%;
   left: ${(props) => (props.left ? '0' : 'unset')};
   right: ${(props) => (props.right ? '0' : 'unset')};
   font-size: 2rem;
-  color: #fff;
+  color: ${(props) => (props.color ? props.color : '#fff')};
 
   &:hover {
     color: #aaa;
@@ -60,7 +51,7 @@ const Slider = styled(FontAwesomeIcon)`
   }
 `;
 
-const Contoller = styled.div`
+export const Contoller = styled.div`
   position: absolute;
   bottom: 5px;
   left: 0;
@@ -87,18 +78,22 @@ const Contoller = styled.div`
   }
 `;
 
-const Info = styled.div`
+export const Info = styled.div`
   padding: 15px;
 
   h1 {
-    font-size: 2rem;
+    font-size: 2.3rem;
+    text-align: center;
   }
 
   p {
+    padding-bottom: 10px;
     font-size: 1.5rem;
+    border-bottom: 1px solid #aaa;
   }
 
   strong {
+    color: rgb(255, 123, 0);
     text-decoration: underline;
   }
 
@@ -117,7 +112,7 @@ const Info = styled.div`
   }
 `;
 
-const InfoIcons = styled.div`
+export const InfoIcons = styled.div`
   margin: 10px 0;
   padding-bottom: 10px;
   display: flex;
@@ -156,154 +151,35 @@ const InfoIcons = styled.div`
   }
 `;
 
-const Features = styled.div`
+export const Features = styled.div`
   margin: 10px;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
   font-size: 1.5rem;
+  text-align: center;
 
   > img {
+    margin: 5px;
     display: block;
   }
 
   img + img {
     margin-left: 10px;
   }
+
+  p {
+    padding: 0;
+    display: block;
+    border: none;
+  }
 `;
 
 const Projects = () => {
-  const [cur, setCur] = useState(0);
-
-  const onClickSlider = (type) => {
-    switch (type) {
-      case 'left':
-        setCur((prev) => prev - 1);
-        break;
-      case 'right':
-        setCur((prev) => prev + 1);
-        break;
-      default:
-    }
-  };
-
-  const onClickController = (i) => {
-    setCur(i);
-  };
-
   return (
     <>
       <StyledProjects>
         <Header>Projects</Header>
         <Inner display="flex" flexFlow="column nowrap" alignItems="center">
-          <Project>
-            <ImgSlide>
-              <img
-                src={MINE_1}
-                alt="minesweeper"
-                style={{ transform: `translateX(-${cur * 100}%)` }}
-              />
-              <img
-                src={MINE_2}
-                alt="minesweeper"
-                style={{ transform: `translateX(-${cur * 100}%)` }}
-              />
-              <img
-                src={MINE_3}
-                alt="minesweeper"
-                style={{ transform: `translateX(-${cur * 100}%)` }}
-              />
-              {cur !== 0 && (
-                <Slider
-                  icon={faChevronLeft}
-                  left="true"
-                  onClick={() => onClickSlider('left')}
-                />
-              )}
-              {cur !== 2 && (
-                <Slider
-                  icon={faChevronRight}
-                  right="true"
-                  onClick={() => onClickSlider('right')}
-                />
-              )}
-              <Contoller>
-                <FontAwesomeIcon
-                  className={cur === 0 ? 'select' : ''}
-                  icon={faCircle}
-                  onClick={() => onClickController(0)}
-                />
-                <FontAwesomeIcon
-                  className={cur === 1 ? 'select' : ''}
-                  icon={faCircle}
-                  onClick={() => onClickController(1)}
-                />
-                <FontAwesomeIcon
-                  className={cur === 2 ? 'select' : ''}
-                  icon={faCircle}
-                  onClick={() => onClickController(2)}
-                />
-              </Contoller>
-            </ImgSlide>
-            <Info>
-              <h1>MineSweeper</h1>
-              <InfoIcons>
-                <div>
-                  <FontAwesomeIcon icon={faUser} />
-                  <span>1인 프로젝트</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faCalendar} />
-                  <span>1주일</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faGithub} />
-                  <span
-                    className="btn-effect"
-                    onClick={() => {
-                      window.open('https://github.com/seodevv/MineSweeper');
-                    }}
-                  >
-                    Source
-                  </span>
-                </div>
-                <div className="big" style={{ marginTop: '10px' }}>
-                  <FontAwesomeIcon icon={faGear} />
-                  <span>주요 기능</span>
-                </div>
-                <div className="big">
-                  <span>
-                    [난이도 설정, 마우스 액션, 인접 빈칸 자동 열림, 시간 측정]
-                  </span>
-                </div>
-                <div className="demo">
-                  <button
-                    className="btn-effect"
-                    onClick={() => {
-                      window.open('https://seodevv.github.io/MineSweeper/');
-                    }}
-                  >
-                    구경 하러 가기
-                  </button>
-                </div>
-              </InfoIcons>
-              <p>
-                React를 처음 접하면서 간단한 웹 게임을 만들어보았습니다.
-                <br />이 게임(지뢰찾기)를 만들면서 <strong>webpack</strong>을
-                사용하여 <strong>모듈 번들링에 대해 이해</strong>할 수 있었고,
-                babel, react-refresh, dev-tools 등 다양한 plugins 와 loader 를
-                활용 할 수 있는 계기가 되었습니다.
-                <br />
-              </p>
-              <Features>
-                <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white" />
-                <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=CSS3&logoColor=white" />
-                <img src="https://img.shields.io/badge/javascript-F7DF1E?style=flat-square&logo=javascript&logoColor=white" />
-                <img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=white" />
-              </Features>
-            </Info>
-          </Project>
+          <MineSweeper />
+          <Tikitaka />
         </Inner>
       </StyledProjects>
     </>
---
+++ src/components/projects/Tikitaka.jsx
@@ -0,0 +1,212 @@
+import {
+  Contoller,
+  Features,
+  ImgSlide,
+  Info,
+  InfoIcons,
+  Project,
+  Slider,
+} from '../Projects';
+import TIKITAKA_1 from '../../img/tikitaka_1.png';
+import TIKITAKA_2 from '../../img/tikitaka_2.png';
+import TIKITAKA_3 from '../../img/tikitaka_3.png';
+import TIKITAKA_4 from '../../img/tikitaka_4.png';
+import TIKITAKA_5 from '../../img/tikitaka_5.png';
+import TIKITAKA_6 from '../../img/tikitaka_6.png';
+import TIKITAKA_7 from '../../img/tikitaka_7.png';
+import TIKITAKA_8 from '../../img/tikitaka_8.png';
+import { useState } from 'react';
+import {
+  faChevronLeft,
+  faChevronRight,
+  faCircle,
+  faGear,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const Tikitaka = () => {
+  const [cur, setCur] = useState(0);
+
+  const images = [
+    TIKITAKA_1,
+    TIKITAKA_2,
+    TIKITAKA_3,
+    TIKITAKA_4,
+    TIKITAKA_5,
+    TIKITAKA_6,
+    TIKITAKA_7,
+    TIKITAKA_8,
+  ];
+
+  const onClickSlider = (type) => {
+    switch (type) {
+      case 'left':
+        setCur((prev) => prev - 1);
+        break;
+      case 'right':
+        setCur((prev) => prev + 1);
+        break;
+      default:
+    }
+  };
+
+  const onClickController = (i) => {
+    setCur(i);
+  };
+
+  return (
+    <>
+      <Project>
+        <ImgSlide height="600px">
+          {images.map((img) => (
+            <img
+              src={img}
+              alt="minesweeper"
+              style={{ transform: `translateX(-${cur * 100}%)` }}
+            />
+          ))}
+          {cur !== 0 && (
+            <Slider
+              icon={faChevronLeft}
+              left="true"
+              color="#000"
+              onClick={() => onClickSlider('left')}
+            />
+          )}
+          {cur !== images.length - 1 && (
+            <Slider
+              icon={faChevronRight}
+              right="true"
+              color="#000"
+              onClick={() => onClickSlider('right')}
+            />
+          )}
+          <Contoller>
+            {images.map((_, i) => (
+              <FontAwesomeIcon
+                className={cur === i ? 'select' : ''}
+                icon={faCircle}
+                onClick={() => onClickController(i)}
+              />
+            ))}
+          </Contoller>
+        </ImgSlide>
+        <Info>
+          <h1>Tikitaka</h1>
+          <InfoIcons>
+            <div>
+              <FontAwesomeIcon icon={faUser} />
+              <span>1인 프로젝트</span>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faCalendar} />
+              <span>23.08</span>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faGithub} />
+              <span
+                className="btn-effect"
+                onClick={() => {
+                  window.open('https://github.com/seodevv/tikitaka');
+                }}
+              >
+                Source
+              </span>
+            </div>
+            <div className="big" style={{ marginTop: '10px' }}>
+              <FontAwesomeIcon icon={faGear} />
+              <span>주요 기능</span>
+            </div>
+            <div className="big">
+              <span>
+                [로그인 기능, OAuth, 회원 가입, 메일 인증, 소셜 기능, 인터랙티브
+                기능, 소셜 검색 기능, 피드 기능, 실시간 채팅 기능, 알람 기능]
+              </span>
+              <p
+                style={{
+                  marginTop: '10px',
+                  fontWeight: 'bold',
+                  textDecoration: 'underline',
+                }}
+              >
+                guest 계정 : guset / Qwer1234!
+              </p>
+            </div>
+            <div className="demo">
+              <button
+                className="btn-effect"
+                onClick={() => {
+                  window.open('https://seodevv.com/');
+                }}
+              >
+                구경 하러 가기
+              </button>
+            </div>
+          </InfoIcons>
+          <p>
+            <strong>
+              사람들과 일상 생활을 공유하고 소통할 수 있는 어플리케이션입니다.
+            </strong>{' '}
+            사람들과 소통하고 공감하는 것을 좋아하는 사람들을 위해 간단한 소셜
+            시스템을 만들었습니다.
+            <br />
+            <br />
+            처음으로 <strong>풀스택</strong>으로 개발한 프로젝트입니다. 이
+            과정에서 특정 요구 사항이 발생했을 때{' '}
+            <strong>
+              client 와 server side 에서 각각 어떻게 처리해야하는지
+            </strong>
+            &nbsp;고민해보고 이해할 수 있는 프로젝트였습니다.
+            <br />
+            <br />
+            <strong>UI & UX</strong> 를 깊게 고민하여 스스로 디자인하였고{' '}
+            <strong>웹 반응형으로 제작되어 데스크탑, 모바일</strong>에서 모두
+            대응 가능합니다. 또한 keyframe 을 활용하여 여러{' '}
+            <strong>애니메이션</strong>들을 구현하였습니다.
+            <br />
+            <br />
+            그리고 <strong>실시간 채팅</strong> 기능을 만들면서{' '}
+            <strong>socket</strong> 에 대한 개념을 더욱 확실히 숙지할 수
+            있었습니다.
+            <br />
+            <br />그 외에도 복수의 페이지, 소셜 로그인, 웹 푸쉬, 서비스워커 등을
+            활용해보면서 <strong>라우팅, OAuth, 알람</strong> 등에 대한 개념을
+            이해할 수 있는 계기가 되었습니다.
+          </p>
+          <Features>
+            <p>Front-End</p>
+            <div>
+              <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white" />
+              <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=CSS3&logoColor=white" />
+              <img src="https://img.shields.io/badge/javascript-F7DF1E?style=flat-square&logo=javascript&logoColor=white" />
+              <img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=white" />
+              <img src="https://img.shields.io/badge/webpack-000000?style=flat-square&logo=webpack&logoColor=white" />
+              <img src="https://img.shields.io/badge/fontawesome-528dd7?style=flat-square&logo=fontawesome&logoColor=white" />
+              <img src="https://img.shields.io/badge/redux-764ABC?style=flat-square&logo=redux&logoColor=white" />
+            </div>
+            <p>Back-End</p>
+            <div>
+              <img src="https://img.shields.io/badge/nodedotjs-339933?style=flat-square&logo=node.js&logoColor=white" />
+              <img src="https://img.shields.io/badge/express-000000?style=flat-square&logo=express&logoColor=white" />
+              <img src="https://img.shields.io/badge/socketdotio-010101?style=flat-square&logo=socketdotio&logoColor=white" />
+              <img src="https://img.shields.io/badge/mariadb-003545?style=flat-square&logo=mariadb&logoColor=white" />
+              <img src="https://img.shields.io/badge/firebase-ffca28?style=flat-square&logo=firebase&logoColor=white" />
+              <img src="https://img.shields.io/badge/ffmpeg-007808?style=flat-square&logo=ffmpeg&logoColor=white" />
+            </div>
+            <p>Deployment</p>
+            <div>
+              <img src="https://img.shields.io/badge/amazonaws-232f3e?style=flat-square&logo=amazonaws&logoColor=white" />
+              <img src="https://img.shields.io/badge/git-f05032?style=flat-square&logo=git&logoColor=white" />
+              <img src="https://img.shields.io/badge/github-181717?style=flat-square&logo=github&logoColor=white" />
+            </div>
+          </Features>
+        </Info>
+      </Project>
+    </>
+  );
+};
+
+export default Tikitaka;
